063a33ab5406954b9ddbd030f78aed7bd9e0f1e2
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2024-10-15 19:58:46 +0300

    Use connector via npm package

--- dapp/src/wagmiConfig.ts
+++ dapp/src/wagmiConfig.ts
@@ -1,8 +1,8 @@
 import { http, createConfig } from "wagmi";
 import { arbitrumNova, arbitrumSepolia } from "wagmi/chains";
-import { sequenceWaasTransportWallet } from "./waasTransportConnector";
+import { sequenceCrossAppEmbeddedWallet } from "@0xsequence/cross-app-embedded-wallet-connector";
 
-const connector = sequenceWaasTransportWallet({
+const connector = sequenceCrossAppEmbeddedWallet({
   projectAccessKey: "AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI",
   walletUrl: "http://localhost:4444",
   chainId: 42170,
