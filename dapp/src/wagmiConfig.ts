51fc880e6a113866ef3011d3cf1aeb7aa690ab31
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2024-10-16 14:50:02 +0300

    Add dapp config params to env vars

--- dapp/src/wagmiConfig.ts
+++ dapp/src/wagmiConfig.ts
@@ -3,8 +3,8 @@ import { arbitrumNova, arbitrumSepolia } from "wagmi/chains";
 import { sequenceCrossAppEmbeddedWallet } from "@0xsequence/cross-app-embedded-wallet-connector";
 
 const connector = sequenceCrossAppEmbeddedWallet({
-  projectAccessKey: "AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI",
-  walletUrl: "http://localhost:4444",
+  projectAccessKey: import.meta.env.VITE_PROJECT_ACCESS_KEY,
+  walletUrl: import.meta.env.VITE_WALLET_URL,
   chainId: 42170,
 });
 
