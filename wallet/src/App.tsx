4be4709a4bdd004696c0df65b6003d2814884faf
Matt Rintoul <matt@matt416.com>
2025-02-19 11:37:13 -0500

    Various UI enhancements
    Fix sign out modal colors
    Fix missing page reference

--- wallet/src/App.tsx
+++ wallet/src/App.tsx
@@ -4,7 +4,7 @@ import { AppLayout, ProtectedLayout } from './Layout'
 import { Auth } from './pages/Auth'
 // import { Home } from './pages/Home'
 import { InventoryPage } from './pages/inventory/InventoryPage'
-import { SendPage } from './pages/SendPage'
+// import { SendPage } from './pages/SendPage'
 import { TransactionsPage } from './pages/TransactionsPage'
 // import { ReceivePage } from './pages/ReceivePage'
 import { ROUTES } from './routes'
@@ -23,7 +23,7 @@ export const App: React.FC = () => {
       <Route element={<ProtectedLayout />}>
         <Route index element={<Home />} />
         <Route path={ROUTES.INVENTORY} element={<InventoryPage />} />
-        <Route path={ROUTES.SEND} element={<SendPage />} />
+        {/* <Route path={ROUTES.SEND} element={<SendPage />} /> */}
         <Route path={ROUTES.TRANSACTIONS} element={<TransactionsPage />} />
         {/* <Route path={ROUTES.HISTORY} element={<HistoryPage />} /> */}
         <Route path={ROUTES.DISCOVER} element={<DiscoverPage />} />
