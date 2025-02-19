489e2b48ee99312d45592f92e197139f84d0a91b
akgunberk <akgunberk@yahoo.com>
2025-02-19 15:36:26 +0300

    add native coin image for the tile

--- wallet/src/pages/inventory/components/token-tile-native-balance.tsx
+++ wallet/src/pages/inventory/components/token-tile-native-balance.tsx
@@ -1,5 +1,5 @@
 import { TokenTile } from './token-tile'
-import { NetworkImage } from '@0xsequence/design-system'
+import { nativeTokenImageUrl, TokenImage } from '@0xsequence/design-system'
 import { formatUnits } from 'ethers'
 
 import type { TokenTileProps } from '../types'
@@ -18,7 +18,7 @@ export function TokenTileNativeBalance(props: TokenTileProps) {
       tokenId={tokenID}
       className="p-4 flex flex-col items-start gap-3"
     >
-      <NetworkImage chainId={chainId} size="lg" />
+      <TokenImage src={nativeTokenImageUrl(chainId, 'lg')} size="lg" withNetwork={chainId} />
       <div className="flex flex-col flex-1 justify-end items-start">
         <span className="text-style-normal font-bold text-seq-grey-500">{title}</span>
         <div>
