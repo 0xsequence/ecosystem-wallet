65dccd1329e5e8103808cd5f1bdaf5a04104de43
Matt Rintoul <matt@matt416.com>
2025-02-27 11:47:20 -0500

    adjust auth color

--- wallet/src/pages/Auth.tsx
+++ wallet/src/pages/Auth.tsx
@@ -306,7 +306,7 @@ function AuthCoverWrapper({ children }: { children: React.ReactNode }) {
   } as React.CSSProperties
   return (
     <div className="flex w-[calc(100%-32px)] md:w-auto mx-4 min-h-[40rem] bg-[#272322] overflow-clip rounded-lg">
-      <div className="grid auth-grid-template max-w-screen-lg w-full bg-black">
+      <div className="grid auth-grid-template max-w-screen-lg w-full bg-[#272322]">
         <div className="flex flex-col items-center flex-1 place-self-center w-full">{children}</div>
         <div
           className="hidden sm:flex flex-col items-end justify-end p-8 flex-shrink [background-image:var(--background)] bg-cover bg-no-repeat"
