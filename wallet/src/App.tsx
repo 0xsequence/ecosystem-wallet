bdc0bc26a7551a879738d919937dca0cc8c53a64
Matt Rintoul <matt@matt416.com>
2025-03-12 09:40:39 -0400

    fix scroll offset issue on navigate

--- wallet/src/App.tsx
+++ wallet/src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router'
+import { Navigate, Route, Routes, useLocation } from 'react-router'
 
 import { AppLayout, ProtectedLayout } from './Layout'
 import { Auth } from './pages/Auth'
@@ -8,26 +8,35 @@ import { DiscoverPage } from './pages/discover'
 import { DiscoverShowRoute } from './pages/discover/show'
 import { WalletConnectModal } from './pages/WalletConnectModal'
 import { TransactionHistory as HistoryPage } from './pages/TransactionHistory/index'
+import { useEffect } from 'react'
 export const App: React.FC = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
   return (
-    <Routes>
-      {/* Public routes */}
-      <Route element={<AppLayout />}>
-        <Route path={ROUTES.AUTH} element={<Auth />} />
-      </Route>
+    <>
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AppLayout />}>
+          <Route path={ROUTES.AUTH} element={<Auth />} />
+        </Route>
 
-      {/* Protected routes */}
-      <Route element={<ProtectedLayout />}>
-        <Route index element={<WalletConnectModal variant="popup" />} />
-        <Route path={ROUTES.INVENTORY} element={<InventoryPage />} />
-        <Route path={ROUTES.DISCOVER} element={<DiscoverPage />} />
-        <Route path={`${ROUTES.DISCOVER}/:id`} element={<DiscoverShowRoute />} />
-        <Route path={ROUTES.MARKET} element={<></>} />
-        <Route path={ROUTES.HISTORY} element={<HistoryPage />} />
-      </Route>
+        {/* Protected routes */}
+        <Route element={<ProtectedLayout />}>
+          <Route index element={<WalletConnectModal variant="popup" />} />
+          <Route path={ROUTES.INVENTORY} element={<InventoryPage />} />
+          <Route path={ROUTES.DISCOVER} element={<DiscoverPage />} />
+          <Route path={`${ROUTES.DISCOVER}/:id`} element={<DiscoverShowRoute />} />
+          <Route path={ROUTES.MARKET} element={<></>} />
+          <Route path={ROUTES.HISTORY} element={<HistoryPage />} />
+        </Route>
 
-      {/* Redirect unknown routes to index */}
-      <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
-    </Routes>
+        {/* Redirect unknown routes to index */}
+        <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
+      </Routes>
+    </>
   )
 }
