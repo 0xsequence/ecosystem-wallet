3d00b689203c8763b9957acecc0f4913532343f4
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2025-03-14 15:51:49 +0300

    Update json case for sanetize, fix overflow for message sig content view

--- wallet/src/utils/sanitize.ts
+++ wallet/src/utils/sanitize.ts
@@ -14,12 +14,30 @@ function escapeHtml(string: string): string {
 }
 
 /**
+ * Checks if a string is valid JSON
+ * @param str - The string to check
+ * @returns boolean indicating if string is valid JSON
+ */
+function isValidJSON(str: string): boolean {
+  try {
+    JSON.parse(str)
+    return true
+  } catch {
+    return false
+  }
+}
+
+/**
  * Recursively sanitizes an object by escaping all string values
  * @param data - The data to sanitize
  * @returns Sanitized copy of the data
  */
 export function sanitize<T>(data: T): T {
   if (typeof data === 'string') {
+    // Don't sanitize if it's a valid JSON string
+    if (isValidJSON(data)) {
+      return data as T
+    }
     return escapeHtml(data) as T
   }
 
